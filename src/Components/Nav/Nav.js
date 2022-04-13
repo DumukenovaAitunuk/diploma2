c190312731708f04c1e20ec025a0ab172ea71d98
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-04-13 13:52:38 +0600

    open new

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import "./Nav.css";
+
+
+function Nav() {
+  return (
+    <nav className="Nav">
+      {/* <Logo /> */}
+      <ul>
+        {/* <NavItem url="/">Home</NavItem>
+        <NavItem url="/products">Products</NavItem>
+        <NavItem url="/contacts">Contacts</NavItem> */}
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
